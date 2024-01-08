4e515d25878594240df788fde0cd212bba59b60c
Harshitha <harshithams016@gmail.com>
2024-01-08 14:42:54 +0530

    Update 5.c

--- 5.c
+++ 5.c
@@ -1,116 +1,135 @@
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-
+#include &lt;stdbool.h&gt;
+#include &lt;stdio.h&gt;
+#include &lt;stdlib.h&gt;
 
 struct Node {
-    int data;
-    struct Node* next;
+int data;
+struct Node* next;
 };
 
 
-void push(struct Node** head_ref, int new_data) {
-  
-    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+void push(struct Node** head_ref, int new_data);
 
-   
-    new_node->data = new_data;
-  
-    new_node->next = (*head_ref);
+/* A utility function to check if
+given data is present in a list */
+bool isPresent(struct Node* head, int data);
 
+/* Function to get union of two
+linked lists head1 and head2 */
+struct Node* getUnion(struct Node* head1,struct Node* head2)
+{
+struct Node* result = NULL;
+struct Node *t1 = head1, *t2 = head2;
 
-    (*head_ref) = new_node;
+// Insert all elements of
+// list1 to the result list
+while (t1 != NULL) {
+push(&amp;result, t1-&gt;data);
+t1 = t1-&gt;next;
+}
+
+// Insert those elements of list2
+// which are not present in result list
+while (t2 != NULL) {
+if (!isPresent(result, t2-&gt;data))
+push(&amp;result, t2-&gt;data);
+t2 = t2-&gt;next;
+}
+
+return result;
+}
+
+/* Function to get intersection of
+two linked lists head1 and head2 */
+struct Node* getIntersection(struct Node* head1,struct Node* head2)
+{
+struct Node* result = NULL;
+struct Node* t1 = head1;
+// Traverse list1 and search each element of it in
+// list2. If the element is present in list 2, then
+// insert the element to result
+while (t1 != NULL) {
+if (isPresent(head2, t1-&gt;data))
+push(&amp;result, t1-&gt;data);
+t1 = t1-&gt;next;
+}
+
+return result;
 }
 
 
-bool isPresent(struct Node* head, int data) {
-    struct Node* t = head;
-    while (t != NULL) {
-        if (t->data == data)
-            return true;
-        t = t->next;
-    }
-    return false;
+
+void push(struct Node** head_ref, int new_data)
+{
+
+struct Node* new_node
+= (struct Node*)malloc(sizeof(struct Node));
+
+
+new_node-&gt;data = new_data;
+
+new_node-&gt;next = (*head_ref);
+
+
+(*head_ref) = new_node;
 }
 
 
-struct Node* getUnion(struct Node* head1, struct Node* head2) {
-    struct Node* result = NULL;
-    struct Node *t1 = head1, *t2 = head2;
-
-
-    while (t1 != NULL) {
-        push(&result, t1->data);
-        t1 = t1->next;
-    }
-
-   
-    while (t2 != NULL) {
-        if (!isPresent(result, t2->data))
-            push(&result, t2->data);
-        t2 = t2->next;
-    }
-
-    return result;
+void printList(struct Node* node)
+{
+while (node != NULL) {
+printf(&quot;%d &quot;, node-&gt;data);
+node = node-&gt;next;
+}
 }
 
 
-struct Node* getIntersection(struct Node* head1, struct Node* head2) {
-    struct Node* result = NULL;
-    struct Node* t1 = head1;
+bool isPresent(struct Node* head, int data)
+{
+struct Node* t = head;
+while (t != NULL) {
+if (t-&gt;data == data)
 
-   
-    while (t1 != NULL) {
-        if (isPresent(head2, t1->data))
-            push(&result, t1->data);
-        t1 = t1->next;
-    }
-
-    return result;
+return 1;
+t = t-&gt;next;
+}
+return 0;
 }
 
 
-void printList(struct Node* node) {
-    while (node != NULL) {
-        printf("%d ", node->data);
-        node = node->next;
-    }
-}
+int main()
+{
+
+struct Node* head1 = NULL;
+struct Node* head2 = NULL;
+struct Node* intersecn = NULL;
+struct Node* unin = NULL;
+
+push(&amp;head1, 20);
+push(&amp;head1, 4);
+push(&amp;head1, 15);
+push(&amp;head1, 10);
 
 
-int main() {
-    
-    struct Node* head1 = NULL;
-    struct Node* head2 = NULL;
-    struct Node* intersecn = NULL;
-    struct Node* unin = NULL;
+push(&amp;head2, 10);
+push(&amp;head2, 2);
+push(&amp;head2, 4);
+push(&amp;head2, 8);
 
-   
-    push(&head1, 20);
-    push(&head1, 4);
-    push(&head1, 15);
-    push(&head1, 10);
+intersecn = getIntersection(head1, head2);
+unin = getUnion(head1, head2);
 
-   
-    push(&head2, 10);
-    push(&head2, 2);
-    push(&head2, 4);
-    push(&head2, 8);
+printf(&quot;\n First list is \n&quot;);
+printList(head1);
 
-    intersecn = getIntersection(head1, head2);
-    unin = getUnion(head1, head2);
+printf(&quot;\n Second list is \n&quot;);
+printList(head2);
 
-    printf("\nFirst list is \n");
-    printList(head1);
+printf(&quot;\n Intersection list is \n&quot;);
+printList(intersecn);
 
-    printf("\nSecond list is \n");
-    printList(head2);
+printf(&quot;\n Union list is \n&quot;);
+printList(unin);
 
-    printf("\nIntersection list is \n");
-    printList(intersecn);
-
-    printf("\nUnion list is \n");
-    printList(unin);
-
-    return 0;
+return 0;
 }
